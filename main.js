1c87981c86acd951d204d6ad6ae2c1d4cbecab31
hcneycup <a.chew0520221@arts.ac.uk>
2025-08-03 23:50:11 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+require('dotenv').config();
+
+let mainWindow;
+
+ipcMain.handle('get-api-key', () => {
+    return process.env.API_KEY;
+});
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 450,
+        height: 350,
+        title: "Daily Football Tracker",
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        titleBarStyle: 'default',
+        resizable: true,
+        show: false
+    });
+
+    mainWindow.loadFile('index.html');
+
+    // Show window when ready
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+
